c61d5b698b078e7e1787a555244b1279996b769b
Yasir Hamid <ratherseenu16@gmail.com>
2025-02-28 10:23:02 +0530

    updated changes

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all React files
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
